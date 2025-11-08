1784d3f343cb3b9868d28a9f667776839d0e9d09
Victor Githinji <novabloodemperor@gmail.com>
2025-11-08 23:41:47 +0300

    Prepare backend for redeploy

--- course_pilot/settings.py
+++ course_pilot/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', 
-    'courses',
+    'course_pilot.courses.apps.CoursesConfig',
 ]
 
 MIDDLEWARE = [
--- course_pilot/urls.py
+++ course_pilot/urls.py
@@ -2,6 +2,4 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('courses.urls')),
-]
+    path('api/', include('course_pilot.courses.urls')),]
