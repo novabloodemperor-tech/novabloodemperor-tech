e59424af4343493d685ec5f3e1bc0f2520a47cb4
Victor Githinji <novabloodemperor@gmail.com>
2025-11-08 23:54:25 +0300

    Add root page to fix 404

--- course_pilot/urls.py
+++ course_pilot/urls.py
@@ -1,5 +1,9 @@
-from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
-    path('api/', include('course_pilot.courses.urls')),]
+    path('admin/', admin.site.urls),  # optional, if you use admin
+    path('api/', include('course_pilot.courses.urls')),  # your API routes
+    path('', lambda request: HttpResponse("Backend is running!")),  # root page
+]
