f2044222adc02b86c8f9bc4ae3397c72817086d0
Victor Githinji <novabloodemperor@gmail.com>
2025-11-11 15:03:25 +0300

    Add Mpesa pay endpoint

--- course_pilot/courses/urls.py
+++ course_pilot/courses/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("pay/", views.pay, name="mpesa-pay"),
+    path('check-eligibility/', views.check_eligibility, name='check_eligibility'),
+    path('pay/', views.pay, name='pay'),  # ✅ Add this line
 ]
-
